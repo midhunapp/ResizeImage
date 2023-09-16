c400092ffa64ad10cb1cc3705d05bc1c99c3bd65
midhunapp <midhunapp@gmail.com>
2023-09-16 19:22:39 +0530

    Add files via upload

---
+++ ResizeImage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ResizeImage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
